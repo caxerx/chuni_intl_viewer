d65f8cf32f16a7c06c72a362deaa8b1c36744a23
Caxer Tsang <caxer.tsang@atg.ai>
2021-11-06 02:30:28 +0800

    feat: chunirec down workaround

--- build.js
+++ build.js
@@ -2,20 +2,21 @@ const path = require("path");
 const { rollup } = require("rollup");
 const nodeResolve = require("@rollup/plugin-node-resolve").default;
 const { terser } = require("rollup-plugin-terser");
+const json = require("@rollup/plugin-json");
 
 console.log("Compiling...");
 
-const build = async() => {
-    const bundle = await rollup({
-        input: path.join(__dirname, "main.js"),
-        plugins: [nodeResolve(), terser()]
-    });
-    await bundle.write({
-        file: path.join(__dirname, "main.min.js"),
-        format: "iife",
-    })
-}
+const build = async () => {
+  const bundle = await rollup({
+    input: path.join(__dirname, "main.js"),
+    plugins: [nodeResolve(), terser(), json()],
+  });
+  await bundle.write({
+    file: path.join(__dirname, "main.min.js"),
+    format: "iife",
+  });
+};
 
 build().then(() => {
-    console.log("Finished");
+  console.log("Finished");
 });
