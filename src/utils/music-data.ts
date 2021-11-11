505258fa2f292d8cddde0b6032309ca7cbbb1e7c
Caxer Tsang <caxer.tsang@atg.ai>
2021-11-12 03:59:03 +0800

    feat: use chunirec

--- src/utils/music-data.ts
+++ src/utils/music-data.ts
@@ -1,6 +1,6 @@
 export async function getIntlData() {
   const data = await fetch(
-    "https://cdn.jsdelivr.net/gh/caxerx/chuni_intl_viewer/chunirec.json"
+    "https://api.chunirec.net/2.0/music/showall.json?region=jp&token=252db1d77e53f52fd85c5b346fef7c90e345b3b3f0b12018a2074298e4b35182"
   ).then((d) => d.json());
   return data;
 }
